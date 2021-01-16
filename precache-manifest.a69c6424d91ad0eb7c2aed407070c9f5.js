cb23bf13364622fb87035866d03efcd5a54ec07a
Hans-lee-2006 <hanslee2006@gmail.com>
2021-01-16 10:20:16 +0800

    deploy

---
+++ precache-manifest.a69c6424d91ad0eb7c2aed407070c9f5.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff812bcb925fa4cb1eb",
+    "url": "/xikxik/css/app.bd871b00.css"
+  },
+  {
+    "revision": "eff812bcb925fa4cb1eb",
+    "url": "/xikxik/css/app.bd871b00.css.map"
+  },
+  {
+    "revision": "30ef3cc4298c623eee01",
+    "url": "/xikxik/css/chunk-vendors.89dcc8ba.css"
+  },
+  {
+    "revision": "30ef3cc4298c623eee01",
+    "url": "/xikxik/css/chunk-vendors.89dcc8ba.css.map"
+  },
+  {
+    "revision": "28b0e2e3e7516fa16cd4056c2b81856a",
+    "url": "/xikxik/favicon-16x16.png"
+  },
+  {
+    "revision": "360bf4d4dc84213d595a7a5fc4c9c113",
+    "url": "/xikxik/favicon-32x32.png"
+  },
+  {
+    "revision": "be95ffa8807954c403e38cadaefb58a0",
+    "url": "/xikxik/favicon.ico"
+  },
+  {
+    "revision": "28b0e2e3e7516fa16cd4056c2b81856a",
+    "url": "/xikxik/favicon.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/xikxik/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/xikxik/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/xikxik/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/xikxik/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/xikxik/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/xikxik/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/xikxik/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/xikxik/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/xikxik/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/xikxik/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/xikxik/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/xikxik/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/xikxik/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/xikxik/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/xikxik/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "858c0b92472a8a4b6fe53790e6eabd07",
+    "url": "/xikxik/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "bee614acc0b81e76fb5bae1412abc821",
+    "url": "/xikxik/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "858c0b92472a8a4b6fe53790e6eabd07",
+    "url": "/xikxik/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "bee614acc0b81e76fb5bae1412abc821",
+    "url": "/xikxik/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "5750756103f1a497a143e3deb74ca220",
+    "url": "/xikxik/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "302dccd3a0ef713905437ac786f080d8",
+    "url": "/xikxik/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "6cc73bbec52b6aee576690e40710832c",
+    "url": "/xikxik/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9916ef756ed3642e1885fe0be38cd503",
+    "url": "/xikxik/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "f1d6558b437c85eae665cc364f8a3686",
+    "url": "/xikxik/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "6cc73bbec52b6aee576690e40710832c",
+    "url": "/xikxik/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "28b0e2e3e7516fa16cd4056c2b81856a",
+    "url": "/xikxik/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "360bf4d4dc84213d595a7a5fc4c9c113",
+    "url": "/xikxik/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "be95ffa8807954c403e38cadaefb58a0",
+    "url": "/xikxik/img/icons/favicon.ico"
+  },
+  {
+    "revision": "53af4cc6c24002e68c31c002065198c7",
+    "url": "/xikxik/img/icons/manifest.json"
+  },
+  {
+    "revision": "c775a22b7081e29d77e0d37f2bcbd78a",
+    "url": "/xikxik/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "4affd3f1e5579587a370e8da1fc6c179",
+    "url": "/xikxik/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "cd60999945f9696dd0aa941afac1e96e",
+    "url": "/xikxik/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/xikxik/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "d2c79ca25051e73ac4b307d3d0935be4",
+    "url": "/xikxik/index.html"
+  },
+  {
+    "revision": "eff812bcb925fa4cb1eb",
+    "url": "/xikxik/js/app.17412111.js"
+  },
+  {
+    "revision": "eff812bcb925fa4cb1eb",
+    "url": "/xikxik/js/app.17412111.js.map"
+  },
+  {
+    "revision": "30ef3cc4298c623eee01",
+    "url": "/xikxik/js/chunk-vendors.e8d72c8d.js"
+  },
+  {
+    "revision": "30ef3cc4298c623eee01",
+    "url": "/xikxik/js/chunk-vendors.e8d72c8d.js.map"
+  },
+  {
+    "revision": "d6588607586f6cbf2ab56dfa02547fef",
+    "url": "/xikxik/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xikxik/robots.txt"
+  },
+  {
+    "revision": "292671c4421ccac7509404b2ad8ee6c6",
+    "url": "/xikxik/static/favicon.png"
+  },
+  {
+    "revision": "cac9eb4a9df8c3237f6fd83ae32c7341",
+    "url": "/xikxik/static/img/og_image.png"
+  }
+]);
